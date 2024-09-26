a5b84acb8ef92bbef4b02d578c93879e07746516
Swapnil Bilgoji <swapnilbilgoji@gmail.com>
2024-09-26 14:41:37 +0530

    change

--- application.py
+++ application.py
@@ -18,6 +18,7 @@ def homePage():
 def index():
     if request.method == 'POST':
         try:
+            
             searchString = request.form['content'].replace(" ","")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
